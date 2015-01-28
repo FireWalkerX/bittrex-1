88313c2f6d5faae2f326dab50547a4f538ed323a
Stu Halliburton <stu.halliburton@gmail.com>
2015-01-27 22:11:17 -0600

    Response: add success and message getters

--- lib/bittrex/api/response.rb
+++ lib/bittrex/api/response.rb
@@ -1,11 +1,17 @@
 module Bittrex
   module Api
     class Response
-      attr_reader :data, :results
+      attr_reader :data, :results, :message
 
       def initialize(data)
         @data = JSON.load(data)
         @results = @data.fetch('result')
+        @success = @data.fetch('success')
+        @message = @data.fetch('message')
+      end
+
+      def success?
+        @success
       end
     end
   end
